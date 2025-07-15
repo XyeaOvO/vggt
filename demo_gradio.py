52a7405915745cc3436c028f346048fe01c11ae4
Xyea <xyeaovo@gmail.com>
2025-07-15 20:10:16 +0800

    Update demo files: use from_pretrained method and fix import path

--- demo_gradio.py
+++ demo_gradio.py
@@ -27,11 +27,11 @@ from vggt.utils.geometry import unproject_depth_map_to_point_map
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 print("Initializing and loading VGGT model...")
-# model = VGGT.from_pretrained("facebook/VGGT-1B")  # another way to load the model
+model = VGGT.from_pretrained("facebook/VGGT-1B")  # another way to load the model
 
-model = VGGT()
-_URL = "https://huggingface.co/facebook/VGGT-1B/resolve/main/model.pt"
-model.load_state_dict(torch.hub.load_state_dict_from_url(_URL))
+# model = VGGT()
+# _URL = "https://huggingface.co/facebook/VGGT-1B/resolve/main/model.pt"
+# model.load_state_dict(torch.hub.load_state_dict_from_url(_URL))
 
 
 model.eval()
--- demo_viser.py
+++ demo_viser.py
@@ -24,7 +24,7 @@ try:
 except ImportError:
     print("onnxruntime not found. Sky segmentation may not work.")
 
-from visual_util import segment_sky, download_file_from_url
+from vggt.test.visual_util import segment_sky, download_file_from_url
 from vggt.models.vggt import VGGT
 from vggt.utils.load_fn import load_and_preprocess_images
 from vggt.utils.geometry import closed_form_inverse_se3, unproject_depth_map_to_point_map
